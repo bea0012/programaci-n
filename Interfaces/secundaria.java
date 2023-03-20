5ae715d59897cceb19751d7a59d9cfb7ad4c8f02
bea0012 <127144945+bea0012@users.noreply.github.com>
2023-03-20 21:03:07 +0100

    Add files via upload

---
+++ Interfaces/secundaria.java
@@ -0,0 +1,105 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+
+public class secundaria extends JFrame {
+
+	private JPanel fondo;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					secundaria frame = new secundaria();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public secundaria() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 667, 488);
+		fondo = new JPanel();
+		fondo.setBackground(new Color(62, 0, 0));
+		fondo.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(fondo);
+		fondo.setLayout(null);
+		
+		JLabel noticia1 = new JLabel("Fall of the Angels");
+		noticia1.setForeground(new Color(255, 255, 255));
+		noticia1.setFont(new Font("Tahoma", Font.BOLD, 12));
+		noticia1.setBounds(124, 392, 106, 14);
+		fondo.add(noticia1);
+		
+		JLabel titulo = new JLabel("Supernatural News");
+		titulo.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 30));
+		titulo.setForeground(new Color(255, 255, 255));
+		titulo.setBounds(187, 31, 294, 46);
+		fondo.add(titulo);
+		
+		JLabel banner = new JLabel("New label");
+		banner.setIcon(new ImageIcon("C:\\Users\\Bea\\Desktop\\1DAW\\Programación\\banner.jpg"));
+		banner.setBounds(0, 0, 651, 103);
+		fondo.add(banner);
+		
+		JLabel textIntroductorio = new JLabel("Congratulations!");
+		textIntroductorio.setFont(new Font("Tahoma", Font.BOLD, 15));
+		textIntroductorio.setForeground(new Color(255, 255, 255));
+		textIntroductorio.setBounds(266, 114, 124, 46);
+		fondo.add(textIntroductorio);
+		
+		JLabel text1 = new JLabel(" You are now subcribed to our portal of Supernatural news");
+		text1.setForeground(new Color(255, 255, 255));
+		text1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		text1.setBounds(170, 156, 325, 14);
+		fondo.add(text1);
+		
+		JLabel text2 = new JLabel("here you have some news to start with");
+		text2.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		text2.setForeground(new Color(255, 255, 255));
+		text2.setBounds(222, 175, 218, 14);
+		fondo.add(text2);
+		
+		JLabel fotoNoticia1 = new JLabel("New label");
+		fotoNoticia1.setIcon(new ImageIcon("C:\\Users\\Bea\\Downloads\\supernatural-823-sacrifice-angels-falling.jpg"));
+		fotoNoticia1.setBounds(41, 212, 275, 175);
+		fondo.add(fotoNoticia1);
+		
+		JLabel textNoticia1 = new JLabel("Last week a strange phenomenon happend all around our country...");
+		textNoticia1.setForeground(new Color(255, 255, 255));
+		textNoticia1.setBounds(21, 413, 328, 14);
+		fondo.add(textNoticia1);
+		
+		JLabel textNoticia2 = new JLabel("A little town in Kansas, named Harlan was evacuated by...");
+		textNoticia2.setForeground(new Color(255, 255, 255));
+		textNoticia2.setBounds(359, 413, 282, 14);
+		fondo.add(textNoticia2);
+		
+		JLabel noticia2 = new JLabel("Lethal virus or raised of death");
+		noticia2.setFont(new Font("Tahoma", Font.BOLD, 12));
+		noticia2.setForeground(new Color(255, 255, 255));
+		noticia2.setBounds(397, 392, 185, 14);
+		fondo.add(noticia2);
+		
+		JLabel fotoNoticia2 = new JLabel("New label");
+		fotoNoticia2.setIcon(new ImageIcon("C:\\Users\\Bea\\Desktop\\1DAW\\Programación\\Map_of_Smith_Co,_Ks,_USA.png"));
+		fotoNoticia2.setBounds(359, 212, 268, 175);
+		fondo.add(fotoNoticia2);
+	}
+}
---
+++ Interfaces/ventana_principal.java
@@ -0,0 +1,263 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Panel;
+import java.awt.Checkbox;
+import java.awt.Label;
+import java.awt.TextField;
+import javax.swing.JTextField;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import java.awt.Color;
+import javax.swing.JCheckBox;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.ImageIcon;
+import javax.swing.JRadioButton;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ContainerAdapter;
+import java.awt.event.ContainerEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JPasswordField;
+
+public class ventana_principal extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField Nombre;
+	private JTextField Apellido;
+	private JTextField nombreUsuario;
+	private JTextField Gmail;
+	private JTextField otraEspecie;
+	static ventana_principal principal;
+	private JPasswordField Contraseña;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					principal = new ventana_principal();
+					principal.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ventana_principal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 669, 488);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(60, 0, 0));
+		contentPane.setToolTipText("dfhndfhn");
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel firstName = new JLabel("First Name");
+		firstName.setBounds(45, 137, 60, 24);
+		firstName.setFont(new Font("Tahoma", Font.BOLD, 11));
+		firstName.setForeground(new Color(255, 255, 255));
+		contentPane.add(firstName);
+		
+		JLabel lastName = new JLabel("Last Name");
+		lastName.setBounds(45, 175, 60, 14);
+		lastName.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lastName.setForeground(new Color(255, 255, 255));
+		contentPane.add(lastName);
+		
+		Nombre = new JTextField();
+		Nombre.setBounds(120, 139, 129, 20);
+		contentPane.add(Nombre);
+		Nombre.setColumns(10);
+		
+		Apellido = new JTextField();
+		Apellido.setBounds(120, 169, 129, 20);
+		contentPane.add(Apellido);
+		Apellido.setColumns(10);
+		
+		nombreUsuario = new JTextField();
+		nombreUsuario.setBounds(120, 231, 129, 20);
+		contentPane.add(nombreUsuario);
+		nombreUsuario.setColumns(10);
+		
+		JLabel userName = new JLabel("User name");
+		userName.setBounds(44, 233, 61, 17);
+		userName.setFont(new Font("Tahoma", Font.BOLD, 11));
+		userName.setForeground(new Color(255, 255, 255));
+		contentPane.add(userName);
+		
+		JLabel password = new JLabel("Password");
+		password.setBounds(54, 265, 56, 14);
+		password.setFont(new Font("Tahoma", Font.BOLD, 11));
+		password.setForeground(new Color(255, 255, 255));
+		contentPane.add(password);
+		
+		JButton inicio = new JButton("Submit");
+		inicio.setBounds(271, 394, 103, 23);
+		inicio.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+					JFrame login = new login();
+					login.setVisible(true);
+					principal.setVisible(false);
+					
+			}
+		});
+		contentPane.add(inicio);
+		
+		JLabel titulo = new JLabel("Supernatural News");
+		titulo.setBounds(187, 31, 294, 46);
+		titulo.setForeground(new Color(255, 255, 255));
+		titulo.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 30));
+		titulo.setBackground(new Color(0, 128, 0));
+		contentPane.add(titulo);
+		
+		JLabel banner = new JLabel("New label");
+		banner.setBounds(0, 0, 650, 102);
+		banner.setBackground(Color.WHITE);
+		banner.setIcon(new ImageIcon("C:\\Users\\Bea\\Desktop\\1DAW\\Programación\\banner.jpg"));
+		contentPane.add(banner);
+		
+		Gmail = new JTextField();
+		Gmail.setBounds(120, 200, 129, 20);
+		contentPane.add(Gmail);
+		Gmail.setColumns(10);
+		
+		JLabel email = new JLabel("Email");
+		email.setBounds(70, 208, 36, 14);
+		email.setFont(new Font("Tahoma", Font.BOLD, 11));
+		email.setForeground(new Color(255, 255, 255));
+		contentPane.add(email);
+		
+		JLabel species = new JLabel("Species");
+		species.setBounds(64, 296, 46, 14);
+		species.setFont(new Font("Tahoma", Font.BOLD, 11));
+		species.setForeground(new Color(255, 255, 255));
+		contentPane.add(species);
+		
+		final JRadioButton boton1 = new JRadioButton("Hunter");
+		boton1.setBounds(120, 292, 71, 23);
+		boton1.setForeground(new Color(255, 255, 255));
+		boton1.setBackground(new Color(62, 0, 0));
+		contentPane.add(boton1);
+		
+		
+		final JRadioButton boton2 = new JRadioButton("Demon");
+		boton2.setBounds(187, 292, 78, 23);
+		boton2.setForeground(new Color(255, 255, 255));
+		boton2.setBackground(new Color(60, 0, 0));
+		contentPane.add(boton2);
+		
+		final JRadioButton boton3 = new JRadioButton("Angel");
+		boton3.setBounds(262, 292, 71, 23);
+		boton3.setForeground(new Color(255, 255, 255));
+		boton3.setBackground(new Color(60, 0, 0));
+		contentPane.add(boton3);
+		
+		final JRadioButton boton4 = new JRadioButton("Other");
+		boton4.setBounds(120, 318, 60, 23);
+		
+		boton1.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				
+				if(boton1.isSelected()) {
+					boton1.setSelected(true);
+					boton2.setSelected(false);
+					boton3.setSelected(false);
+					boton4.setSelected(false);
+				}
+			}
+		});
+		
+		boton2.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				
+				if(boton2.isSelected()) {
+					boton1.setSelected(false);
+					boton2.setSelected(true);
+					boton3.setSelected(false);
+					boton4.setSelected(false);
+				}
+			
+			}
+		});
+		
+		boton3.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				
+				if(boton3.isSelected()) {
+					boton1.setSelected(false);
+					boton2.setSelected(false);
+					boton3.setSelected(true);
+					boton4.setSelected(false);
+				}
+			}
+		});
+		
+		boton4.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				
+				if(boton4.isSelected()) {
+					boton1.setSelected(false);
+					boton2.setSelected(false);
+					boton3.setSelected(false);
+					boton4.setSelected(true);
+				}
+			
+			if(!boton4.isSelected()) {
+				otraEspecie.enable();
+				otraEspecie.hide();
+			}else {
+				otraEspecie.enable(true);
+				otraEspecie.show();
+			}
+			
+		}
+		
+		});
+		boton4.setForeground(new Color(255, 255, 255));
+		boton4.setBackground(new Color(55, 0, 0));
+		contentPane.add(boton4);
+		
+		otraEspecie = new JTextField();
+		otraEspecie.setBounds(180, 319, 118, 20);
+		otraEspecie.setEnabled(false);
+		contentPane.add(otraEspecie);
+		otraEspecie.setColumns(10);
+		
+		final JRadioButton botonContraseña = new JRadioButton("Show");
+		botonContraseña.setBounds(255, 261, 109, 23);
+		botonContraseña.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				
+				if(botonContraseña.isSelected()) {
+					Contraseña.setEchoChar((char)0);
+				}else {
+					Contraseña.setEchoChar('*');
+				}
+			}
+		});
+		botonContraseña.setForeground(new Color(255, 255, 255));
+		botonContraseña.setBackground(new Color(53, 0, 0));
+		contentPane.add(botonContraseña);
+		
+		Contraseña = new JPasswordField();
+		Contraseña.setBounds(120, 262, 129, 20);
+		contentPane.add(Contraseña);
+	}
+}
